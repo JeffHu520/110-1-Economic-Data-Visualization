b5d8edef444bfce311c6f4fd415fb90135c1eb6f
JeffHu520 <91120369+JeffHu520@users.noreply.github.com>
2021-11-24 15:28:17 +0800

    Update Week10Practice.R

--- Week10Practice.R
+++ Week10Practice.R
@@ -1,43 +1,48 @@
 df = list()
 df$LuRouFan = data.frame(
-  x = c(1 ,1),
-  y = c(39 ,117),
-  fill = factor(
-    c("滷肉飯" ,"魯肉飯" ) ,levels = c("魯肉飯" ,"滷肉飯" )
+    x = c(1 ,1),
+    y = c(39 ,117),
+    fill = factor(
+      c("滷肉飯" ,"魯肉飯" ) ,levels = c("魯肉飯" ,"滷肉飯" )
+    )
   )
-)
 
 df$FriedChicken = data.frame(
-  x = c(2 ,2),
-  y = c(95 ,68),
-  fill = factor(
-    c("鹽酥雞" ,"鹹酥雞" ) ,levels = c("鹽酥雞" ,"鹹酥雞" )
+    x = c(2 ,2),
+    y = c(68 ,95),
+    fill = factor(
+      c("鹹酥雞","鹽酥雞" ) ,levels = c("鹽酥雞","鹹酥雞" )
+    )
   )
-)
 
 df$ChickenRice = data.frame(
-  x = c(3 ,3),
-  y = c(269 ,174),
-  fill = factor(
-    c("雞肉飯" ,"火雞肉飯" ) ,levels = c("雞肉飯" ,"火雞肉飯" )
+    x = c(3 ,3),
+    y = c(174 ,269),
+    fill = factor(
+      c("火雞肉飯","雞肉飯") ,levels = c("雞肉飯" ,"火雞肉飯" )
+    )
   )
-)
 df$GuaBao = data.frame(
-  x = c(4 ,4),
-  y = c(4 ,23),
-  fill = factor(
-    c("割包" ,"刈包" ) ,levels = c("刈包" ,"割包" )
+    x = c(4 ,4),
+    y = c(4 ,23),
+    fill = factor(
+      c("割包" ,"刈包" ) ,levels = c("刈包" ,"割包" )
+    )
   )
-)
 df$PotStickers = data.frame(
-  x = c(5 ,5),
-  y = c(122 ,16),
-  fill = factor(
-    c("鍋貼" ,"煎餃" ) ,levels = c("鍋貼" ,"煎餃" )
+    x = c(5 ,5),
+    y = c(16 ,122),
+    fill = factor(
+      c("煎餃","鍋貼") ,levels = c("鍋貼" ,"煎餃" )
+    )
   )
-)
 
-dx = -0.2 #input$dx
+```
+
+
+```{r}
+
+dx = -0.25 #input$dx
 dy = -20 #input$dy
 h = 0.2 #input$h
 
@@ -96,7 +101,7 @@ ggplot()+
     ),
     color = "#010d85",
     position = position_dodge(width = 1),
-    size = 8 #input$size
+    size = 15 #input$size
   )+
   geom_text(
     data = df$FriedChicken,
@@ -107,7 +112,7 @@ ggplot()+
     ),
     color = "#010d85",
     position = position_dodge(width = 1),
-    size = 8 
+    size = 15
   )+
   geom_text(
     data = df$ChickenRice,
@@ -118,7 +123,7 @@ ggplot()+
     ),
     color = "#010d85",
     position = position_dodge(width = 1),
-    size = 8
+    size = 15
   )+
   geom_text(
     data = df$GuaBao,
@@ -129,7 +134,7 @@ ggplot()+
     ),
     color = "#010d85",
     position = position_dodge(width = 1),
-    size = 8 
+    size = 15 
   )+
   geom_text(
     data = df$PotStickers,
@@ -140,7 +145,7 @@ ggplot()+
     ),
     color = "#010d85",
     position = position_dodge(width = 1),
-    size = 8 
+    size = 15
   )+
   geom_text(
     data = df$LuRouFan,
@@ -150,7 +155,7 @@ ggplot()+
       label = y
     ),
     color = "black",
-    size = 6
+    size = 10
   )+
   geom_text(
     data = df$FriedChicken,
@@ -160,7 +165,7 @@ ggplot()+
       label = y
     ),
     color = "black",
-    size = 6
+    size = 10
   )+
   geom_text(
     data = df$ChickenRice,
@@ -170,7 +175,7 @@ ggplot()+
       label = y
     ),
     color = "black",
-    size = 6
+    size = 10
   )+
   geom_text(
     data = df$GuaBao,
@@ -180,7 +185,7 @@ ggplot()+
       label = y
     ),
     color = "black",
-    size = 6
+    size = 10
   )+
   geom_text(
     data = df$PotStickers,
@@ -190,7 +195,7 @@ ggplot()+
       label = y
     ),
     color = "black",
-    size = 6
+    size = 10
   )+
   coord_flip()+
   theme_void()+
@@ -199,7 +204,9 @@ ggplot()+
     subtitle = "常見相似小吃比較",
     caption = "經濟部商業司 商工登記公示資料查詢"
   )+
+  xlim(1-1, 5+1)+
   theme(
     legend.position = "none",
-    plot.title = element_text(hjust = 0.5),
-    plot.subtitle = element_text(hjust = 0.5))
+    plot.title = element_text(size = 50,hjust = 0.5),
+    plot.subtitle = element_text(size = 40,hjust = 0.5),
+    plot.caption = element_text(size = 25))
